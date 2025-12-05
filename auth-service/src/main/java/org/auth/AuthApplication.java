adfeaa3a7116114413317659892dc7e344cf56e1
brian <brian506@naver.com>
2025-12-05 18:02:05 +0900

    feat : 체팅 알림 기능 구현

--- auth-service/src/main/java/org/auth/AuthApplication.java
+++ auth-service/src/main/java/org/auth/AuthApplication.java
@@ -9,12 +9,14 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @EnableJpaAuditing
 @EnableDiscoveryClient
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @Import(RedisConfig.class)
 @ComponentScan(basePackages = {"org.auth", "org.common"})
+@EnableRedisRepositories(basePackages = "org.auth.domain.repository.redis")
 public class AuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class,args);
